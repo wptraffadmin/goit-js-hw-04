07f445ba475d5dd04b0d3dc39ba8e60ad07ba990
wptraffadmin <wptraffadmin@proton.me>
2024-12-30 15:21:44 +0200

    JS-4

---
+++ js/task-1.js
@@ -0,0 +1,21 @@
+function isEnoughCapacity(products, containerSize) {
+  const totalProducts = Object.values(products).reduce((sum, count) => sum + count, 0);
+
+  return totalProducts <= containerSize;
+}
+
+console.log(
+  isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)
+); // true
+
+console.log(
+  isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)
+); // false
+
+console.log(
+  isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)
+); // true
+
+console.log(
+  isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)
+); // false
